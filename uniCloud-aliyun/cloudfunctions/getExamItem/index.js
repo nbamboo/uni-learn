b7d37432986e26390a6dbbbde091599b19bc3a57
nbamboo <nbamboo@hotmail.com>
2024-03-04 22:14:13 +0800

    20240304

--- uniCloud-aliyun/cloudfunctions/getExamItem/index.js
+++ uniCloud-aliyun/cloudfunctions/getExamItem/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const db = uniCloud.database();
 exports.main = async (event, context) => {
-	let res = await db.collection('exam_items').get();
+	let res = await db.collection('exam_items').field().get();
 	return res;
 };
